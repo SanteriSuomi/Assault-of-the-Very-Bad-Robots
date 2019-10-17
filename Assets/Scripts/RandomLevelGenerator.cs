b699205574d70cc92916dddeb3d96fce2871a029
SanteriSuomi <sant.suomi@gmail.com>
2019-10-18 01:25:59 +0300

    random generator wip

--- Assets/Scripts/RandomLevelGenerator.cs
+++ Assets/Scripts/RandomLevelGenerator.cs
@@ -15,17 +15,16 @@ public class RandomLevelGenerator : MonoBehaviour
 
     private GameObject[,,] map;
 
-    private int startPassArrayIntX;
-    private int startPassArrayIntZ;
-
     private void Awake()
     {
+        Random.InitState((int)System.DateTime.Now.Ticks);
         map = new GameObject[xLength, Mathf.RoundToInt(yLength + yLength), zLength];
     }
 
     private void Start()
     {
         InitializeMapBlocks();
+        InitializePath();
     }
 
     private void InitializeMapBlocks()
@@ -35,66 +34,62 @@ public class RandomLevelGenerator : MonoBehaviour
         {
             GameObject xObject = Instantiate(normalBlock);
             xObject.transform.position = new Vector3(xRow, 0, 0);
-
             // Add map array for further processing.
             map[xRow, 0, 0] = xObject;
-
             // Instantiate every zRow.
             for (int zRow = 0; zRow < zLength; zRow++)
             {
                 GameObject zObject = Instantiate(normalBlock);
                 zObject.transform.position = new Vector3(xRow, 0, zRow);
-
                 // Add map array for further processing.
                 map[xRow, 0, zRow] = zObject;
-
                 // Instantiate every yRow.
                 for (float yRow = 0; yRow < yLength; yRow += 0.5f)
                 {
                     GameObject yObject = Instantiate(normalBlock);
                     yObject.transform.position = new Vector3(xRow, yRow + 0.5f, zRow);
-
                     // Add map array for further processing.
                     map[xRow, Mathf.RoundToInt(yRow + yRow), zRow] = yObject;
                 }
             }
         }
-
-        InitializePath();
     }
 
     private void InitializePath()
     {
-        int randomStartPass = Random.Range(1, 19);
-        int randomStartPassAmount = Random.Range(4, 8);
+        int randomStartPassX = Random.Range(5, 14);
+        int randomStartPassAmountZ = Random.Range(4, 8);
+        Debug.Log(randomStartPassX);
+        Debug.Log(randomStartPassAmountZ);
 
-        for (int i = 0; i < randomStartPassAmount; i++)
+        for (int i = 0; i < randomStartPassAmountZ; i++)
         {
-            startPassArrayIntX = i;
-            Destroy(map[randomStartPass, 0, i]);
+            Destroy(map[randomStartPassX, 0, i]);
         }
 
-        print(startPassArrayIntX);
+        int randomSecondPassAmountX = Random.Range(3, 6);
+        int randomSecondPassDirection = Random.Range(0, 2);
+        Debug.Log(randomSecondPassAmountX);
+        Debug.Log(randomSecondPassDirection);
+
+        for (int i = 0; i < randomSecondPassAmountX; i++)
+        {
+            if (randomSecondPassDirection == 0)
+            {
+                print("+");
+                Destroy(map[randomStartPassX + i, 0, randomStartPassAmountZ]);
+            }
+            else
+            {
+                print("-");
+                Destroy(map[randomStartPassX - i, 0, randomStartPassAmountZ]);
+            }
+        }
         
-        int randomSecondPassAmount = Random.Range(4, 8);
-        for (int i = 0; i < randomSecondPassAmount; i++)
+        int randomThirdPassAmountZ = Random.Range(4, 8);
+        for (int i = 0; i < randomThirdPassAmountZ; i++)
         {
-            Destroy(map[startPassArrayIntX + i, 0, startPassArrayIntZ]);
+            Destroy(map[randomStartPassX, 0, i]);
         }
-        
-        /*
-        Vector3 startPos = map[Random.Range(0, xLength), (int)yLength, 0].transform.position;
-        print(startPos);
-        Vector3 endPos = map[Random.Range(0, xLength), (int)yLength, zLength - 1].transform.position;
-        print(endPos);
-        float distance = Vector3.Distance(startPos, endPos);
-        print(distance);
-
-        RaycastHit[] raycastHits = Physics.RaycastAll(startPos, endPos, distance);
-        foreach (var hit in raycastHits)
-        {
-            Destroy(hit.collider.gameObject);
-        }
-        */
     }
 }
