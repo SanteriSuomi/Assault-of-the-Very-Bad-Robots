846409f8533a808e1d1045b684f077a52c737683
SanteriSuomi <sant.suomi@gmail.com>
2019-10-25 23:45:26 +0300

    code refactoring wip 3

--- Assets/Scripts/RandomLevelGenerator.cs
+++ Assets/Scripts/RandomLevelGenerator.cs
@@ -98,30 +98,15 @@ public class RandomLevelGenerator : MonoBehaviour
             // Instantiate the X row of level blocks.
             for (int xRow = 0; xRow < xLength; xRow++)
             {
-                GameObject xObject = Instantiate(levelPathPrefab);
-                xObject.transform.parent = levelPrefabParent;
-                xObject.layer = 9;
-                xObject.transform.position = new Vector3(xRow, 0, 0);
-                // Add objects to the multi-dimensional array of level blocks.
-                map[xRow, 0, 0] = xObject;
+                InitializeXRow(xRow);
                 // For every X row, instantiate a Z row.
                 for (int zRow = 0; zRow < zLength; zRow++)
                 {
-                    GameObject zObject = Instantiate(levelPathPrefab);
-                    zObject.transform.parent = levelPrefabParent;
-                    zObject.layer = 9;
-                    zObject.transform.position = new Vector3(xRow, 0, zRow);
-                    map[xRow, 0, zRow] = zObject;
+                    InitializeZRow(xRow, zRow);
                     // For every X and Z row, instantiate Y row.
                     for (float yRow = 0; yRow < yLength; yRow += 0.5f)
                     {
-                        GameObject yObject = Instantiate(levelPrefab);
-                        yObject.transform.parent = levelPrefabParent;
-                        yObject.layer = 11;
-                        // Make sure the objects above the level ground are carving for the navmesh.
-                        yObject.AddComponent<NavMeshObstacle>().carving = true;
-                        yObject.transform.position = new Vector3(xRow, yRow + 0.5f, zRow);
-                        map[xRow, Mathf.RoundToInt(yRow + yRow), zRow] = yObject;
+                        InitializeYRow(xRow, zRow, yRow);
                     }
                 }
             }
@@ -132,6 +117,36 @@ public class RandomLevelGenerator : MonoBehaviour
             Debug.Log(e);
             #endif
         }
+    }
+
+    private void InitializeXRow(int xRow)
+    {
+        GameObject xObject = Instantiate(levelPathPrefab);
+        xObject.transform.parent = levelPrefabParent;
+        xObject.layer = 9;
+        xObject.transform.position = new Vector3(xRow, 0, 0);
+        // Add objects to the multi-dimensional array of level blocks.
+        map[xRow, 0, 0] = xObject;
+    }
+
+    private void InitializeZRow(int xRow, int zRow)
+    {
+        GameObject zObject = Instantiate(levelPathPrefab);
+        zObject.transform.parent = levelPrefabParent;
+        zObject.layer = 9;
+        zObject.transform.position = new Vector3(xRow, 0, zRow);
+        map[xRow, 0, zRow] = zObject;
+    }
+
+    private void InitializeYRow(int xRow, int zRow, float yRow)
+    {
+        GameObject yObject = Instantiate(levelPrefab);
+        yObject.transform.parent = levelPrefabParent;
+        yObject.layer = 11;
+        // Make sure the objects above the level ground are carving for the navmesh.
+        yObject.AddComponent<NavMeshObstacle>().carving = true;
+        yObject.transform.position = new Vector3(xRow, yRow + 0.5f, zRow);
+        map[xRow, Mathf.RoundToInt(yRow + yRow), zRow] = yObject;
     }
     #endregion
 
@@ -144,72 +159,18 @@ public class RandomLevelGenerator : MonoBehaviour
     {
         try
         {
-            int firstPassX = Random.Range(2, xLength - 2);
-            int firstPassAmountZ = Random.Range(2, zLength / 2);
-            for (int i = 0; i < firstPassAmountZ; i++)
-            {
-                SetAgentStartPoint(firstPassX);
-                Destroy(map[firstPassX, 0, i]);
-            }
-
+            GenerateFirstPass(out int firstPassX, out int firstPassAmountZ);
+            // Spawn the enemy ship.
             SpawnShip();
-
+            // Indicate that agentStartPoint can be set again.
             setAgentStartPoint = false;
-
-            int secondPassAmountX = Random.Range(2, xLength - 2);
-            int secondPassDirection = Random.Range(0, 2);
-            for (int i = 0; i < secondPassAmountX; i++)
-            {
-                if (secondPassDirection == 0)
-                {
-                    thirdPassX = 0;
-                    thirdPassX = firstPassX + i;
-                    Destroy(map[firstPassX + i, 0, firstPassAmountZ]);
-                }
-                else
-                {
-                    thirdPassX = 0;
-                    thirdPassX = firstPassX - i;
-                    Destroy(map[firstPassX - i, 0, firstPassAmountZ]);
-                }
-            }
-
-            int thirdPassAmountZ = Random.Range(2, zLength / 2);
-            for (int i = 0; i < thirdPassAmountZ; i++)
-            {
-                fourthPassZ = 0;
-                fourthPassZ = firstPassAmountZ + i;
-                Destroy(map[thirdPassX, 0, firstPassAmountZ + i]);
-            }
-
-            int fourthPassAmountX = Random.Range(2, xLength - 2);
-            int fourthPassDirection = Random.Range(0, 2);
-            for (int i = 0; i < fourthPassAmountX; i++)
-            {
-                if (fourthPassDirection == 0)
-                {
-                    fifthPassX = 0;
-                    fifthPassX = thirdPassX + i;
-                    Destroy(map[thirdPassX + i, 0, fourthPassZ]);
-                }
-                else
-                {
-                    fifthPassX = thirdPassX - i;
-                    Destroy(map[thirdPassX - i, 0, fourthPassZ]);
-                }
-            }
-
-            int fifthPassAmountZ = zLength - fourthPassZ;
-            Vector3 agentEndPoint = Vector3.zero;
-            for (int i = 0; i < fifthPassAmountZ; i++)
-            {
-                Destroy(map[fifthPassX, 0, fourthPassZ + i]);
-
-                agentEndPoint = SetAgentEndPoint(agentEndPoint, i);
-            }
-
+            GenerateSecondPass(firstPassX, firstPassAmountZ);
+            GenerateThirdPass(firstPassAmountZ);
+            GenerateFourthPass();
+            Vector3 agentEndPoint = GenerateFifthPassAndGetAgentEndPoint();
+            // Spawn the player base at the agent end point (last point on the path).
             SpawnBase(agentEndPoint);
-
+            // Set the enemy agent end point.
             LevelData.Instance.AgentEndPoint = agentEndPoint;
         }
         catch (System.Exception e)
@@ -217,19 +178,115 @@ public class RandomLevelGenerator : MonoBehaviour
             #if UNITY_EDITOR
             Debug.Log(e);
             #endif
-
+            // If the generation algorithm goes off the array, generate the map again.
             GenerateMap();
         }
     }
 
+    private void GenerateFirstPass(out int firstPassX, out int firstPassAmountZ)
+    {
+        // Generate the first pass of the path.
+        // Starting from X position on the array.
+        firstPassX = Random.Range(2, xLength - 2);
+        // Agent start point should be the first position in the path.
+        SetAgentStartPoint(firstPassX);
+        // How much we need to carve in Z axis.
+        firstPassAmountZ = Random.Range(2, zLength / 2);
+        for (int i = 0; i < firstPassAmountZ; i++)
+        {
+            // Carve the path in the array.
+            Destroy(map[firstPassX, 0, i]);
+        }
+    }
+
+    private void GenerateSecondPass(int firstPassX, int firstPassAmountZ)
+    {
+        // Second pass for generating the path.
+        // How much should we generate.
+        int secondPassAmountX = Random.Range(2, xLength - 2);
+        // Direction of the carve on X axis.
+        int secondPassDirection = Random.Range(0, 2);
+        for (int i = 0; i < secondPassAmountX; i++)
+        {
+            // Determine the direction.
+            if (secondPassDirection == 0)
+            {
+                // Start the pass at 0.
+                thirdPassX = 0;
+                // Carve the path starting from the firstpass ending position.
+                thirdPassX = firstPassX + i;
+                Destroy(map[firstPassX + i, 0, firstPassAmountZ]);
+            }
+            else
+            {
+                thirdPassX = 0;
+                thirdPassX = firstPassX - i;
+                Destroy(map[firstPassX - i, 0, firstPassAmountZ]);
+            }
+        }
+    }
+
+    private void GenerateThirdPass(int firstPassAmountZ)
+    {
+        // Third pass amount in Z axis.
+        int thirdPassAmountZ = Random.Range(2, zLength / 2);
+        for (int i = 0; i < thirdPassAmountZ; i++)
+        {
+            fourthPassZ = 0;
+            // Start the fourth pass at the Z position from first pass.
+            fourthPassZ = firstPassAmountZ + i;
+            Destroy(map[thirdPassX, 0, firstPassAmountZ + i]);
+        }
+    }
+
+    private void GenerateFourthPass()
+    {
+        // Fourth pass, again in X axis on different direction.
+        int fourthPassAmountX = Random.Range(2, xLength - 2);
+        int fourthPassDirection = Random.Range(0, 2);
+        for (int i = 0; i < fourthPassAmountX; i++)
+        {
+            if (fourthPassDirection == 0)
+            {
+                fifthPassX = 0;
+                fifthPassX = thirdPassX + i;
+                Destroy(map[thirdPassX + i, 0, fourthPassZ]);
+            }
+            else
+            {
+                fifthPassX = thirdPassX - i;
+                Destroy(map[thirdPassX - i, 0, fourthPassZ]);
+            }
+        }
+    }
+
+    private Vector3 GenerateFifthPassAndGetAgentEndPoint()
+    {
+        // Last generation pass, going to the end of the map in Z axis.
+        // Make sure to generate the path to the end of the map.
+        int fifthPassAmountZ = zLength - fourthPassZ;
+        // Initialize the agent end point with 0.
+        Vector3 agentEndPoint = Vector3.zero;
+        for (int i = 0; i < fifthPassAmountZ; i++)
+        {
+            Destroy(map[fifthPassX, 0, fourthPassZ + i]);
+            // Make sure the agent end point is the last path block to be generated.
+            agentEndPoint = SetCalculateAgentEndPoint(agentEndPoint, i);
+        }
+
+        return agentEndPoint;
+    }
+
     private void SpawnShip()
     {
+        // If the ship exists currently, destroy it.
         if (shipPrefab != null)
         {
             Destroy(enemyBase);
         }
 
         enemyBase = Instantiate(shipPrefab);
+        // Correctly rotate the ship prefab.
         enemyBase.transform.rotation = Quaternion.Euler(0, 270, 0);
         enemyBase.transform.position = LevelData.Instance.AgentStartPoint + new Vector3(0.08f, 0, -2.15f);
     }
@@ -250,6 +307,7 @@ public class RandomLevelGenerator : MonoBehaviour
     #region Other Map Methods
     private void ClearMap()
     {
+        // When generating a new map, destroy all the blocks currently existing.
         foreach (GameObject block in map)
         {
             Destroy(block);
@@ -258,22 +316,27 @@ public class RandomLevelGenerator : MonoBehaviour
 
     private void InitializeSeed()
     {
+        // Initialize the random seed with a value from system time.
         Random.InitState((int)System.DateTime.Now.Ticks);
     }
 
     private void GenerateNavMesh()
     {
+        // Build the agent navmesh.
         navMesh.BuildNavMesh();
     }
 
     private void ForceCleanUp()
     {
+        // Clean all the garbage before starting a game to prevent accidental hitches.
         System.GC.Collect();
+        // Make sure there is no unused resources loaded.
         Resources.UnloadUnusedAssets();
     }
 
     private void SetAgentStartPoint(int firstPassX)
     {
+        // Set the start point for the enemy agents.
         if (!setAgentStartPoint)
         {
             setAgentStartPoint = true;
@@ -281,8 +344,9 @@ public class RandomLevelGenerator : MonoBehaviour
         }
     }
 
-    private Vector3 SetAgentEndPoint(Vector3 agentEndPoint, int i)
+    private Vector3 SetCalculateAgentEndPoint(Vector3 agentEndPoint, int i)
     {
+        // Calculate the agent endpoint from the fifth pass in the path generation.
         if (map[fifthPassX, 0, fourthPassZ + i].transform.position.z > agentEndPoint.z)
         {
             agentEndPoint = map[fifthPassX, 0, fourthPassZ + i].transform.position;
